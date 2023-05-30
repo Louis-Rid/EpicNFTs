37876db93696ac7a58cbaea0122e4b077019f8ef
Louis-Rid <131224467+Louis-Rid@users.noreply.github.com>
2023-05-30 08:27:18 -0600

    Added functionality to deploy on a testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,22 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const QUICKNODE_API_KEY = process.env.QUICK_NODE_API_KEY;
+const PRIVATE_ACCOUNT_KEY = process.env.PRIVATE_ACCOUNT_KEY;
+const PROD_QUICKNODE_KEY = process.env.PROD_QUICKNODE_KEY;
+
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    sepolia: {
+      url: QUICKNODE_API_KEY,
+      accounts: [PRIVATE_ACCOUNT_KEY],
+    },
+    // mainnet: {
+    //   url: PROD_QUICKNODE_KEY,
+    //   accounts: [PRIVATE_ACCOUNT_KEY],
+    // },
+  },
 };
