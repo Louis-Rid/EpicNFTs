0bf7437c882016258f51ba497b378453ed297841
Louis-Rid <131224467+Louis-Rid@users.noreply.github.com>
2023-06-01 08:50:44 -0600

    Added etherscan verification

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,6 +6,7 @@ require("dotenv").config();
 const QUICKNODE_API_KEY = process.env.QUICK_NODE_API_KEY;
 const PRIVATE_ACCOUNT_KEY = process.env.PRIVATE_ACCOUNT_KEY;
 const PROD_QUICKNODE_KEY = process.env.PROD_QUICKNODE_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 module.exports = {
   solidity: "0.8.17",
@@ -18,5 +19,8 @@ module.exports = {
     //   url: PROD_QUICKNODE_KEY,
     //   accounts: [PRIVATE_ACCOUNT_KEY],
     // },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
   },
 };
